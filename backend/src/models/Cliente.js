ec2146c18d90b410a15a9210f478256b45361712
Virissimo Djedjo <djedjovirissimo450@.com>
2025-12-27 20:41:47 -0300

    feat: define Cliente model based on SQL table

--- backend/src/models/Cliente.js
+++ backend/src/models/Cliente.js
@@ -1,1 +1,19 @@
+import { DataTypes } from "sequelize";
 import sequelize from "../config/database";
+
+const Cliente = sequelize.define("Cliente",{
+    id_cliente: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome:{
+        type: DataTypes.STRING(150),
+        allowNull: false
+    },
+    telefone: DataTypes.STRING(20),
+    email: DataTypes.STRING(150),
+    endereco: DataTypes.STRING(255)
+});
+
+export default Cliente;
