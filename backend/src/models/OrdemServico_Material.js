b8a9772066ce8604a8b5aae18b895bf8dfa9979a
Virissimo Djedjo <djedjovirissimo450@.com>
2025-12-28 00:45:29 -0300

    feat: cria o modelo OrdemServico_Material com mapeamento do banco de dados

---
+++ backend/src/models/OrdemServico_Material.js
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+const OrdemServico_Material = sequelize.define("OrdemServico_Material", {
+    id_os: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    id_material: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    quantidade: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    preco_unitario: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    }
+}, {
+    tableName: 'OrdemServico_Material',
+});
+
+export default OrdemServico_Material;
