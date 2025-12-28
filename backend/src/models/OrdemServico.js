54a1780d7d770d6eea4940d3d78154ddda0b2d9d
Virissimo Djedjo <djedjovirissimo450@.com>
2025-12-27 23:54:13 -0300

    feat: cria o modelo OrdemServico com mapeamento do banco de dados

---
+++ backend/src/models/OrdemServico.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+const OrdemServico = sequelize.define("OrdemServico",{
+    id_os:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    data_abertura:{
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    data_fechamento:{
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+     status:{
+        type: DataTypes.ENUM("ABERTA", "EM_ANDAMENTO", "FINALIZADA", "CANCELADA"),
+        allowNull: false
+     },
+     descricao_problema:{
+        type: DataTypes.TEXT,
+        allowNull: false
+     },
+     valor_total:{
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.00
+     },
+     
+},{
+    tableName: "OrdemServico"
+});
+
+export default OrdemServico;
