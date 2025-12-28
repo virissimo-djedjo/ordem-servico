dafd222ad92867a8f1fe40530740e4fc14e4341b
Virissimo Djedjo <djedjovirissimo450@.com>
2025-12-28 00:29:15 -0300

    feat: cria o modelo Material com mapeamento do banco de dados

---
+++ backend/src/models/Material.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+const Material = sequelize.define("Material",{
+    id_material:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome:{
+        type: DataTypes.STRING(150),
+        allowNull: false
+    },
+    preco_unitario:{
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    estoque:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    }
+},{
+    tableName: "Material"
+});
+
+export default Material;
