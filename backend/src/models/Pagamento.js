7f85c54f333aedb1db473a44533cf97d4a68a43c
Virissimo Djedjo <djedjovirissimo450@.com>
2025-12-28 00:54:48 -0300

    feat: cria o modelo Pagamento com mapeamento do banco de dados

---
+++ backend/src/models/Pagamento.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+const Pagamento = sequelize.define("Pagamento", {
+    id_pagamento: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    id_os: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    forma_pagamento: {
+        type: DataTypes.ENUM('DINHEIRO', 'CARTAO', 'PIX', 'TRANSFERENCIA'),
+        allowNull: false
+    },
+    valor_pago: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    data_pagamento: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    tableName: 'Pagamento'
+});
+
+export default Pagamento;
