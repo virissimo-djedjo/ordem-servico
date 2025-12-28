59c120c17378235723c34d1557d9a9dad8bbd922
Virissimo Djedjo <djedjovirissimo450@.com>
2025-12-28 00:59:20 -0300

    feat cria um modelo de tabela Historico no banco de dados

--- backend/src/models/Historico.js
+++ backend/src/models/Historico.js
@@ -1,2 +1,27 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database";
+
+const Historico = sequelize.define("Historico", {
+    id_historico: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    id_os: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    acao: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    data_hora: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+}, {
+    tableName: 'Historico',
+});
+
+export default Historico;
