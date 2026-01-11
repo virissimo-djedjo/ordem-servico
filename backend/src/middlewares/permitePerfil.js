1160297ca1f97b3f029fab5961d69197e889abf7
Virissimo Djedjo <djedjovirissimo450@.com>
2026-01-11 02:04:49 -0300

    adiciona comentario

--- backend/src/middlewares/permitePerfil.js
+++ backend/src/middlewares/permitePerfil.js
@@ -6,3 +6,16 @@ export default function permitePerfil(...perfisPermitidos) {
     return next();
   };
 }
+
+/*
+O includes() é um método do JavaScript (para Arrays e Strings) 
+que funciona como um detector. Ele serve para responder 
+a uma pergunta simples: "Isso que eu estou procurando está aqui dentro?
+
+const perfisPermitidos = ["ADM", "TECNICO"];
+const perfilDoUsuario = "TECNICO";
+
+const resultado = perfisPermitidos.includes(perfilDoUsuario); 
+// resultado será true, porque "TECNICO" está na lista.
+
+*/
